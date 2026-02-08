fb94f0bae1cdc60c6a8691024b347dc9a3d5b88c
conjon42 <155610998+conjon42@users.noreply.github.com>
2026-02-08 23:15:28 +0300

    undo

--- code/nf_genome_assembly/launch_nf_genome_assembly.sh
+++ code/nf_genome_assembly/launch_nf_genome_assembly.sh
@@ -1,11 +1,3 @@
-#! /usr/bin/bash
-#SBATCH --job-name=genome_assembly
-#SBATCH --error=result.err
-#SBATCH --ntasks=1          # Run a single task
-#SBATCH --cpus-per-task=2   # Number of CPU cores per task
-#SBATCH --mem=4G            # Total memory (4GB)
-#SBATCH --time=24:00:00     # Time limit hrs:min:sec
-
 module load singularity
 module load nextflow
 
