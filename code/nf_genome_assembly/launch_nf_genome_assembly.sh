796315e1ff4af18789556cd3d1106b426905cf62
conjon42 <155610998+conjon42@users.noreply.github.com>
2026-02-08 23:03:54 +0300

    Added hifiasm parameters and metadata to assembly launch script

---
+++ code/nf_genome_assembly/launch_nf_genome_assembly.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/bash
+module load singularity
+module load nextflow
+
+nextflow run nf-core/genomeassembler \
+    -r dev \
+    -profile kaust \
+    --input samplesheet.csv \
+    --outdir ../../results/asssembly \
+    --use_ref \
+    --hifi \
+    --lima \
+    --pacbio_primers ../../data/20260114_bcl_hifi_release/2_A01/common/ \
+    --assembler hifiasm \
+    --hifiasm_args "-l0 --hg-size 320m --primary" \
+    --scaffold_ragtag \
