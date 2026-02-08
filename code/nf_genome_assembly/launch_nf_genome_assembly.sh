82a187dd474e7e589a92e77eae97aa340a1a601c
conjon42 <155610998+conjon42@users.noreply.github.com>
2026-02-08 23:19:42 +0300

    Attempt to fix broken filepaths in launch script

--- code/nf_genome_assembly/launch_nf_genome_assembly.sh
+++ code/nf_genome_assembly/launch_nf_genome_assembly.sh
@@ -4,12 +4,12 @@ module load nextflow
 nextflow run nf-core/genomeassembler \
     -r dev \
     -profile kaust \
-    --input samplesheet.csv \
+    --input ./samplesheet.csv \
     --outdir ../../results/asssembly \
     --use_ref \
     --hifi \
     --lima \
-    --pacbio_primers ../../data/20260114_bcl_hifi_release/2_A01/common/ \
+    --pacbio_primers ../../../data/20260114_bcl_hifi_release/2_A01/common/barcodes.fasta \
     --assembler hifiasm \
     --hifiasm_args "-l0 --hg-size 320m --primary" \
     --scaffold_ragtag \
