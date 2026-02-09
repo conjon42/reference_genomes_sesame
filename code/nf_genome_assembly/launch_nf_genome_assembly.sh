a6d93272734a18710bb3c05e695816b4a8512f02
conjon42 <155610998+conjon42@users.noreply.github.com>
2026-02-09 18:57:22 +0300

    remove lima from nf run

--- code/nf_genome_assembly/launch_nf_genome_assembly.sh
+++ code/nf_genome_assembly/launch_nf_genome_assembly.sh
@@ -10,8 +10,6 @@ nextflow run nf-core/genomeassembler \
     --outdir ../../results/asssembly \
     --use_ref \
     --hifi \
-    --lima \
-    --pacbio_primers ../../data/20260114_bcl_hifi_release/2_A01/common/barcodes.fasta \
     --assembler hifiasm \
     --hifiasm_args "-l0 --hg-size 320m --primary" \
     --scaffold_ragtag
