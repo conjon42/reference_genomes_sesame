48c4d114ab16d7ff163846905954c4360c395afb
conjon42 <conner.johnson@kaust.edu.sa>
2026-02-11 15:46:56 +0300

    Added sbatch script to nf launch script

--- code/nf_genome_assembly/launch_nf_genome_assembly.sh
+++ code/nf_genome_assembly/launch_nf_genome_assembly.sh
@@ -1,5 +1,15 @@
 #! /usr/bin/bash
 
+#SBATCH --time=12-00:00:00
+#SBATCH --job-name=nf_head_genomeassembler
+#SBATCH --memory=4GB
+#SBATCH --partition=interactive # Partition to run the head job (or a dedicated 'head' queue if available)
+#SBATCH --mem=4GB # Job memory request
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=johnsoc@kaust.edu.sa
+
 module load singularity
 module load nextflow
 
