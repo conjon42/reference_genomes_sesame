d05f29576096df0df4c8dc8362b7ba8a5e94c081
conjon42 <conner.johnson@kaust.edu.sa>
2026-02-11 16:10:52 +0300

    Added rome constraint, fixed sbatch script in launcher

--- code/nf_genome_assembly/launch_nf_genome_assembly.sh
+++ code/nf_genome_assembly/launch_nf_genome_assembly.sh
@@ -2,13 +2,13 @@
 
 #SBATCH --time=12-00:00:00
 #SBATCH --job-name=nf_head_genomeassembler
-#SBATCH --memory=4GB
 #SBATCH --partition=interactive # Partition to run the head job (or a dedicated 'head' queue if available)
 #SBATCH --mem=4GB # Job memory request
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=johnsoc@kaust.edu.sa
+#SBATCH --constraint=rome
 
 module load singularity
 module load nextflow
