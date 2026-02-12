81b160146db8928a3f70750ba1618bae95d265ef
conjon42 <155610998+conjon42@users.noreply.github.com>
2026-02-12 10:12:09 +0300

    Added large hifiasm request config to project

--- code/nf_genome_assembly/launch_nf_genome_assembly.sh
+++ code/nf_genome_assembly/launch_nf_genome_assembly.sh
@@ -2,11 +2,10 @@
 
 #SBATCH --time=12-00:00:00
 #SBATCH --job-name=nf_head_genomeassembler
-#SBATCH --memory=4GB
 #SBATCH --partition=interactive # Partition to run the head job (or a dedicated 'head' queue if available)
-#SBATCH --mem=4GB # Job memory request
+#SBATCH --mem=6GB # Job memory request
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=johnsoc@kaust.edu.sa
 
@@ -16,6 +15,7 @@ module load nextflow
 nextflow run nf-core/genomeassembler \
     -r dev \
     -profile kaust \
+    -c ./hifiasm.config \
     --input ./samplesheet.csv \
     --outdir ../../results/asssembly \
     --use_ref \
