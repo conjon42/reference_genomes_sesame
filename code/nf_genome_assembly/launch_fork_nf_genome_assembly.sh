70f7566cc58b6616a836bd3a87e83ae9edf98d1d
conjon42 <conner.johnson@kaust.edu.sa>
2026-02-16 18:05:18 +0300

    Alternate fork of nextflow assembler with primary contigs instead of unitigs as output downstream

---
+++ code/nf_genome_assembly/launch_fork_nf_genome_assembly.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/bash
+
+#SBATCH --time=5-00:00:00
+#SBATCH --job-name=nf_head_genomeassembler
+#SBATCH --partition=interactive # Partition to run the head job (or a dedicated 'head' queue if available)
+#SBATCH --mem=6GB # Job memory request
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=conner.johnson@kaust.edu.sa
+
+module load singularity
+module load nextflow
+
+nextflow run conjon42/genomeassembler \
+    -profile kaust \
+    -c ./hifiasm.config \
+    --input ./samplesheet.csv \
+    --outdir ../../results/asssembly \
+    --use_ref \
+    --hifi \
+    --assembler hifiasm \
+    --hifiasm_args "-l0 --hg-size 320m --primary" \
+    --scaffold_ragtag \
+    -resume
