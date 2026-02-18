059aff582851e87949a9cdb06616f8bfd11bc366
conjon42 <conner.johnson@kaust.edu.sa>
2026-02-18 13:13:43 +0300

    First working draft of compleasm script, one sample only with odb12

---
+++ code/compleasm/compleasm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=compleasm
+#SBATCH --mem=32G
+#SBATCH --cpus-per-task=32
+#SBATCH --output=./logs/compleasm_%j.out
+#SBATCH --error=./logs/compleasm_%j.err
+#SBATCH --time=2:00:00
+#SBATCH --partition=batch
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=conner.johnson@kaust.edu.sa
+
+
+source /ibex/user/johnsoc/miniforge/etc/profile.d/conda.sh
+conda activate compleasm
+
+mkdir ./tmp
+gunzip -c /ibex/project/c2141/sesame/reference_genome_assembly/results/2OW4BC1lvLRFRF_scaffold/giza/assembly/hifiasm/fasta/giza.p_ctg.fa.gz > ./tmp/giza_uncompressed.fa
+
+compleasm run \
+-a ./tmp/giza_uncompressed.fa \
+-o ../../results/2OW4BC1lvLRFRF_scaffold/giza/QC/compleasm \
+-l viridiplantae \
+--odb odb12 \
+-t 32 \
+-L /ibex/user/johnsoc/busco_dbs
+odb
+rm -rf ./tmp
